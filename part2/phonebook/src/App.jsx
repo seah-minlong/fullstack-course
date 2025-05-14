b42eb4bc1fed8fe460d6171b00a5f062c59b26c1
seah-minlong <86313670+seah-minlong@users.noreply.github.com>
2025-05-14 22:45:06 +0800

    Exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,38 +1,41 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from 'axios';
 import Filter from "./Filter.jsx";
 import PersonForm from "./PersonForm.jsx";
 import Persons from "./Persons.jsx";
 
 const App = () => {
-	const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456', id: 1 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-    ]);
+	const [persons, setPersons] = useState([]);
 	const [newName, setNewName] = useState("");
     const [newNumber, setNewNumber] = useState("");
     const [newFilter, setNewFilter] = useState("");
 
-	const addPerson = (event) => {
-		event.preventDefault();
-        
-        const check = persons.filter(p => p.name === newName).length === 0;
-        if (check) {
+    const addPerson = (event) => {
+        event.preventDefault();
+
+        const isDuplicate = persons.some(p => p.name === newName);
+        if (isDuplicate) {
+            alert(`${newName} is already added to phonebook`);
+        } else {
             const personObject = {
                 name: newName,
                 number: newNumber,
                 id: String(persons.length + 1),
             };
-
             setPersons(persons.concat(personObject));
-        } else {
-            alert(`${newName} is already added to phonebook`)
         }
 
         setNewName("");
         setNewNumber("");
     };
+
+    useEffect(() => {
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => {
+                setPersons(response.data)
+            })
+    })
 
 	const handleNameChange = (event) => {
 		setNewName(event.target.value);
