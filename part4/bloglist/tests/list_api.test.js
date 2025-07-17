7fc4466bae19c30b263fbe9e7d7060d5ca267fe8
seah-minlong <86313670+seah-minlong@users.noreply.github.com>
2025-07-17 22:10:01 +0800

    4.12

--- part4/bloglist/tests/list_api.test.js
+++ part4/bloglist/tests/list_api.test.js
@@ -4,20 +4,20 @@ const assert = require("node:assert");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
-const helper = require('./test_helper');
+const helper = require("./test_helper");
 const Blog = require("../models/bloglist");
 
 const api = supertest(app);
 
 beforeEach(async () => {
-    await Blog.deleteMany({})
-    
-    let blogObject = new Blog(helper.initialBlog[0])
-    await blogObject.save()
+	await Blog.deleteMany({});
 
-    blogObject = new Blog(helper.initialBlog[1])
-    await blogObject.save()
-})
+	let blogObject = new Blog(helper.initialBlog[0]);
+	await blogObject.save();
+
+	blogObject = new Blog(helper.initialBlog[1]);
+	await blogObject.save();
+});
 
 test("blogs are returned as json", async () => {
 	await api
@@ -32,69 +32,98 @@ test("all blogs are returned", async () => {
 });
 
 test("Unique identifier property of blog is named id", async () => {
-    const response = await api.get('/api/blogs');
-    
-    const sampleBlog = response.body[0];
-    assert.ok(sampleBlog.hasOwnProperty("id"));
-    assert.ok(!sampleBlog.hasOwnProperty("_id"));
+	const response = await api.get("/api/blogs");
+
+	const sampleBlog = response.body[0];
+	assert.ok(sampleBlog.hasOwnProperty("id"));
+	assert.ok(!sampleBlog.hasOwnProperty("_id"));
 });
 
-test('a specific blog can be viewed', async () => {
-  const blogsAtStart = await helper.blogsInDb()
-  const blogToView = blogsAtStart[0]
+test("a specific blog can be viewed", async () => {
+	const blogsAtStart = await helper.blogsInDb();
+	const blogToView = blogsAtStart[0];
 
-  const resultBlog = await api
-    .get(`/api/blogs/${blogToView.id}`)
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+	const resultBlog = await api
+		.get(`/api/blogs/${blogToView.id}`)
+		.expect(200)
+		.expect("Content-Type", /application\/json/);
 
-  assert.deepStrictEqual(resultBlog.body, blogToView)
-})
+	assert.deepStrictEqual(resultBlog.body, blogToView);
+});
 
-test('a valid blog can be added', async () => {
-    const newBlog = {
+test("a valid blog can be added", async () => {
+	const newBlog = {
 		_id: "5a422b3a1b54a676234d17f9",
 		title: "Test",
 		author: "Edsger W. Dijkstra",
 		url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
 		likes: 12,
 		__v: 0,
-	}
+	};
 
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
+	await api
+		.post("/api/blogs")
+		.send(newBlog)
+		.expect(201)
+		.expect("Content-Type", /application\/json/);
 
-    const blogAtEnd = await helper.blogsInDb()
-    
-    const response = await api.get('/api/blogs')
-    assert.strictEqual(response.body.length, helper.initialBlog.length + 1)
+	const blogAtEnd = await helper.blogsInDb();
 
-    const titles = blogAtEnd.map(b => b.title)
-    assert(titles.includes('Test'))
-})
+	const response = await api.get("/api/blogs");
+	assert.strictEqual(response.body.length, helper.initialBlog.length + 1);
 
-test('default 0 for missing likes property', async () => {
-    const newBlog = {
+	const titles = blogAtEnd.map((b) => b.title);
+	assert(titles.includes("Test"));
+});
+
+test("default 0 for missing likes property", async () => {
+	const newBlog = {
 		_id: "5a422b3a1b54a676234d17f9",
 		title: "Canonical string reduction",
 		author: "Edsger W. Dijkstra",
 		url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-		likes: 12,
 		__v: 0,
-	}
+	};
 
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
+	await api
+		.post("/api/blogs")
+		.send(newBlog)
+		.expect(201)
+		.expect("Content-Type", /application\/json/);
 
-    const response = await api.get('/api/blogs')
-    assert.strictEqual(response.body.length, helper.initialBlog.length + 1)
-})
+	const response = await api
+		.get(`/api/blogs/${newBlog._id}`)
+		.expect(200)
+		.expect("Content-Type", /application\/json/);
+
+	assert.strictEqual(response.body.likes, 0);
+});
+
+test("Missing title Blog not added", async () => {
+	const missingTitleBlog = {
+		author: "Edsger W. Dijkstra",
+		url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+	};
+
+	await api.post("/api/blogs").send(missingTitleBlog).expect(400);
+
+	const blogAtEnd = await helper.blogsInDb();
+
+	assert.strictEqual(blogAtEnd.length, helper.initialBlog.length);
+});
+
+test("Missing url Blog not added", async () => {
+	const missingUrlBlog = {
+		author: "Edsger W. Dijkstra",
+		title: "Test",
+	};
+
+	await api.post("/api/blogs").send(missingUrlBlog).expect(400);
+
+	const blogAtEnd = await helper.blogsInDb();
+
+	assert.strictEqual(blogAtEnd.length, helper.initialBlog.length);
+});
 
 after(async () => {
 	await mongoose.connection.close();
