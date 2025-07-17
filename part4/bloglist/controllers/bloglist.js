eb00faf0d9f9353a452b1619a03136221337743b
seah-minlong <86313670+seah-minlong@users.noreply.github.com>
2025-07-17 17:22:58 +0800

    4.10 - 4.11

--- part4/bloglist/controllers/bloglist.js
+++ part4/bloglist/controllers/bloglist.js
@@ -13,4 +13,17 @@ bloglistRouter.post("/", async (request, response) => {
     response.status(201).json(result);
 });
 
+bloglistRouter.get(`/:id`, async (request, response, next) => {
+    try {
+        const blog = await Blog.findById(request.params.id)
+        if (blog) {
+            response.json(blog);
+        } else {
+            response.status(404).end()
+        }
+    } catch (exception) {
+        next(exception)
+    }
+})
+
 module.exports = bloglistRouter;
--- part4/bloglist/tests/list_api.test.js
+++ part4/bloglist/tests/list_api.test.js
@@ -4,34 +4,18 @@ const assert = require("node:assert");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
+const helper = require('./test_helper');
 const Blog = require("../models/bloglist");
 
 const api = supertest(app);
 
-const initialBlog = [
-    {
-		_id: "5a422a851b54a676234d17f7",
-		title: "React patterns",
-		author: "Michael Chan",
-		url: "https://reactpatterns.com/",
-		likes: 7,
-		__v: 0,
-	},
-	{
-		_id: "5a422aa71b54a676234d17f8",
-		title: "Go To Statement Considered Harmful",
-		author: "Edsger W. Dijkstra",
-		url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-		likes: 5,
-		__v: 0,
-	},
-]
-
 beforeEach(async () => {
     await Blog.deleteMany({})
-    let blogObject = new Blog(initialBlog[0])
+    
+    let blogObject = new Blog(helper.initialBlog[0])
     await blogObject.save()
-    blogObject = new Blog(initialBlog[1])
+
+    blogObject = new Blog(helper.initialBlog[1])
     await blogObject.save()
 })
 
@@ -44,7 +28,7 @@ test("blogs are returned as json", async () => {
 
 test("all blogs are returned", async () => {
 	const response = await api.get("/api/blogs");
-	assert.strictEqual(response.body.length, initialBlog.length);
+	assert.strictEqual(response.body.length, helper.initialBlog.length);
 });
 
 test("Unique identifier property of blog is named id", async () => {
@@ -55,7 +39,44 @@ test("Unique identifier property of blog is named id", async () => {
     assert.ok(!sampleBlog.hasOwnProperty("_id"));
 });
 
+test('a specific blog can be viewed', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+  const blogToView = blogsAtStart[0]
+
+  const resultBlog = await api
+    .get(`/api/blogs/${blogToView.id}`)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  assert.deepStrictEqual(resultBlog.body, blogToView)
+})
+
 test('a valid blog can be added', async () => {
+    const newBlog = {
+		_id: "5a422b3a1b54a676234d17f9",
+		title: "Test",
+		author: "Edsger W. Dijkstra",
+		url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+		likes: 12,
+		__v: 0,
+	}
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const blogAtEnd = await helper.blogsInDb()
+    
+    const response = await api.get('/api/blogs')
+    assert.strictEqual(response.body.length, helper.initialBlog.length + 1)
+
+    const titles = blogAtEnd.map(b => b.title)
+    assert(titles.includes('Test'))
+})
+
+test('default 0 for missing likes property', async () => {
     const newBlog = {
 		_id: "5a422b3a1b54a676234d17f9",
 		title: "Canonical string reduction",
@@ -72,7 +93,7 @@ test('a valid blog can be added', async () => {
         .expect('Content-Type', /application\/json/)
 
     const response = await api.get('/api/blogs')
-    assert.strictEqual(response.body.length, initialBlog.length + 1)
+    assert.strictEqual(response.body.length, helper.initialBlog.length + 1)
 })
 
 after(async () => {
